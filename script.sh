a21f1aecc33aac49db0940400840900b222bf720
atzawada <atzawada@gmail.com>
2019-08-25 15:06:48 -0400

    Get script running

--- script.sh
+++ script.sh
@@ -1,12 +1,21 @@
+#!/bin/ash
+
+ls
+
 cd $REPO_LOCATION
 
-RESULT=$(git diff --name-only HEAD HEAD~1 | grep $TRIGGER_FILE)
+ls
 
-if [ -z $RESULT ]
-then
+RESULT=$(git diff --name-only HEAD HEAD~1 | grep $TRIGGER_FILE)
+echo "$RESULT"
+
+if [ -z $RESULT ]; then
+    dockerd &
+    echo "$BUILD_LOCATION"
     cd $BUILD_LOCATION
     docker build -t $IMAGE_NAME $DOCKERFILE_LOCATION
-    sudo docker push $IMAGE_NAME
+    docker push $IMAGE_NAME
 else
     echo "No changes in file"
 fi
+
