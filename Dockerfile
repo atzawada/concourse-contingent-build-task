fa9ee14d12cc7344bacc74a402740cfc12317fbd
atzaw <atzawada@gmail.com>
2019-08-25 15:10:38 -0400

    Add build script

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM docker:git
 MAINTAINER atzawada@gmail.com
 
 COPY script.sh /bin/script
+COPY build.sh /bin/build
 
 RUN chmod +x /bin/script
-
+RUN chmod +X /bin/build
--- script.sh
+++ script.sh
@@ -10,11 +10,7 @@ RESULT=$(git diff --name-only HEAD HEAD~1 | grep $TRIGGER_FILE)
 echo "$RESULT"
 
 if [ -z $RESULT ]; then
-    dockerd &
-    echo "$BUILD_LOCATION"
-    cd $BUILD_LOCATION
-    docker build -t $IMAGE_NAME $DOCKERFILE_LOCATION
-    docker push $IMAGE_NAME
+    sh build
 else
     echo "No changes in file"
 fi
